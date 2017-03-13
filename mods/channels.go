bbe5ec678593c36ad39537e6c0cd604e56cb3a87
Danny Krainas <dkrainas87@gmail.com>
2017-03-12 23:50:18 -0400

    remove mods package

--- mods/channels.go
+++
@@ -1,9 +0,0 @@
-package mods
-
-type ChannelMap map[string]*Channel
-
-type Channel struct {
-	Alias    string `json:"alias"`
-	Protocol string `json:"protocol"`
-	Endpoint string `json:"endpoint"`
-}
