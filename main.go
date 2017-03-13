94c420d79aece3dfef357a95d3121e2fae8ede9e
Danny Krainas <dkrainas87@gmail.com>
2017-03-13 00:06:59 -0400

    update root command reference in main

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/danielkrainas/gobag/context"
 
 	_ "github.com/danielkrainas/shexd/cmd/api"
-	"github.com/danielkrainas/shexd/cmd/registry"
+	"github.com/danielkrainas/shexd/cmd/root"
 	_ "github.com/danielkrainas/shexd/cmd/version"
 	_ "github.com/danielkrainas/shexd/storage/driver/inmemory"
 )
@@ -27,9 +27,9 @@ func main() {
 
 	rand.Seed(time.Now().Unix())
 	ctx := acontext.WithVersion(acontext.Background(), appVersion)
-	ctx = context.WithValue(ctx, "app.name", strings.Title(registry.Info.Use))
+	ctx = context.WithValue(ctx, "app.name", strings.Title(root.Info.Use))
 
-	dispatch := cmd.CreateDispatcher(ctx, registry.Info)
+	dispatch := cmd.CreateDispatcher(ctx, root.Info)
 	if err := dispatch(); err != nil {
 		log.Fatalln(err)
 	}
