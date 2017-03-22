8c98b34c46796fc38203758fce13f6904172f6bf
Danny Krainas <dkrainas87@gmail.com>
2017-03-22 00:24:19 -0400

    add inmemory blobs driver support

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/danielkrainas/gobag/cmd"
 	"github.com/danielkrainas/gobag/context"
 
+	_ "github.com/danielkrainas/shexd/blobs/driver/inmemory"
 	_ "github.com/danielkrainas/shexd/cmd/api"
 	"github.com/danielkrainas/shexd/cmd/root"
 	_ "github.com/danielkrainas/shexd/cmd/version"
